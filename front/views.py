510b8d9f08342f3ac380934c8588419f25efa3ff
isnou <nouioua0islem@gmail.com>
2021-12-03 19:35:23 +0100

    islem

--- front/views.py
+++ front/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("matromatic.com is on the way")
+    return HttpResponse("matromatic the best choice for your industry")
