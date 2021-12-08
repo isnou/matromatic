f046c8cba6c311ecebb818a847c9e56f729176b7
isnou <nouioua0islem@gmail.com>
2021-12-08 14:06:43 +0100

    latest

--- matromatic/settings.py
+++ matromatic/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'front',
 ]
 
 MIDDLEWARE = [
