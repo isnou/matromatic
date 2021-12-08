6d9ceab7232b8c0ec659a3b074d06a6732c73bc2
isnou <nouioua0islem@gmail.com>
2021-12-08 13:34:24 +0100

    islem nouioua

--- front/views.py
+++ front/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-    template = loader.get_template('font/base.html')
+    template = loader.get_template('base.html')
     return HttpResponse(template.render(request=request))
