1e3939448597d542eec96671b3a60b913e4c0bb5
isnou <nouioua0islem@gmail.com>
2021-12-08 14:10:45 +0100

    hhh

--- front/views.py
+++ front/views.py
@@ -3,5 +3,4 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-    template = loader.get_template('base.html')
-    return HttpResponse(template.render(request=request))
+    return render(request,"base.html")
