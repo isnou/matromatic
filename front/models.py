a010d860aa345e8ff97e2140f8fda77adce4b3b2
isnou <nouioua0islem@gmail.com>
2021-12-22 23:29:11 +0100

    title

--- front/models.py
+++ front/models.py
@@ -11,6 +11,7 @@ class Profile(models.Model):
     intro_big_text = models.CharField(max_length=200)
     intro_small_text = models.CharField(max_length=500)
     intro_button = models.CharField(max_length=50)
+    about_title = models.CharField(max_length=500)
     about_text = models.CharField(max_length=500)
     about_link = models.CharField(max_length=150)
     about_disc = models.CharField(max_length=100)
