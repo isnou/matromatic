edecc41a0aafb2818128e3eb7f6c42ecde54e418
isnou <nouioua0islem@gmail.com>
2021-12-22 23:18:55 +0100

    lot

--- front/views.py
+++ front/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from django.http import Http404
+from .models import Profile
+
 def index(request):
-    return render(request,"base.html")
+    profile_id = 1
+    try:
+        profile = Profile.objects.get(pk=profile_id)
+    except Question.DoesNotExist:
+        raise Http404("Profile does not exist")
+
+    return render(request, 'base.html', {'profile': profile})
