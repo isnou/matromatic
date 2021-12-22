627cdc934afebe0995db2ca1bc42b7e790a7f1a9
isnou <nouioua0islem@gmail.com>
2021-12-22 22:50:05 +0100

    profile

--- front/models.py
+++ front/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Profil(models.Model):
+    name = models.CharField(max_length=200)
+    about = models.CharField(max_length=50)
+    projects = models.CharField(max_length=50)
+    news = models.CharField(max_length=50)
+    contact = models.CharField(max_length=50)
+    intro_big_text = models.CharField(max_length=200)
+    intro_small_text = models.CharField(max_length=500)
+    intro_button = models.CharField(max_length=50)
+    about_text = models.CharField(max_length=500)
+    about_link = models.CharField(max_length=150)
+    about_disc = models.CharField(max_length=100)
