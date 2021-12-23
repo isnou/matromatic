d0af16007e6a93eb13d562a103d195c59d1365bd
isnou <nouioua0islem@gmail.com>
2021-12-23 02:59:41 +0100

    mea culpa

--- front/views.py
+++ front/views.py
@@ -3,12 +3,13 @@ from django.http import HttpResponse
 from django.template import loader
 
 from django.http import Http404
-from .models import Profile# , SiteManager
+from .models import Profile, News# , SiteManager
 
 def index(request):
     try:
         profile = Profile.objects.get(pk=1)
+        news = News.objects.get(pk=1)
     except Profile.DoesNotExist:
         raise Http404("Profile does not exist")
 
-    return render(request, 'base.html', {'profile': profile})
+    return render(request, 'base.html', {'profile': profile}, {'news': news})
