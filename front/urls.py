cb8fb9f54edf1ec1db74b668a6d5bdba1399211d
isnou <nouioua0islem@gmail.com>
2021-12-23 18:53:05 +0100

    ff

--- front/urls.py
+++ front/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('news/', views.news, name='news'),
     path('projects/', views.projects, name='projects'),
 ]
