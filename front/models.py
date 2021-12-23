84e13c953467ed02058b3f90d2834a66f399f475
isnou <nouioua0islem@gmail.com>
2021-12-23 16:24:34 +0100

    blank

--- front/models.py
+++ front/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 
 class Project(models.Model):
     project_name = models.CharField(max_length=50)
-    description = models.TextField(max_length=200, default='blank')
+    description = models.TextField(max_length=200, blank=True)
     left = models.BooleanField()
     photo = models.ImageField(upload_to='project/', height_field=None, width_field=None, max_length=100)
 
@@ -21,7 +21,7 @@ class Project(models.Model):
 
 class News(models.Model):
     news_name = models.CharField(max_length=50)
-    description = models.TextField(max_length=200, default='blank')
+    description = models.TextField(max_length=200, blank=True)
     photo = models.ImageField(upload_to='news/', height_field=None, width_field=None, max_length=100)
 
     class Meta:
@@ -32,28 +32,28 @@ class News(models.Model):
 
 
 class Profile(models.Model):
-    profile_name = models.CharField(max_length=50, default='blank')
+    profile_name = models.CharField(max_length=50, blank=True)
     about = models.CharField(max_length=50)
     projects = models.CharField(max_length=50)
     news = models.CharField(max_length=50)
     contact = models.CharField(max_length=50)
-    intro_big_text = models.TextField(max_length=200, default='blank')
-    intro_small_text = models.TextField(max_length=500, default='blank')
+    intro_big_text = models.TextField(max_length=200, blank=True)
+    intro_small_text = models.TextField(max_length=500, blank=True)
     intro_button = models.CharField(max_length=50)
-    about_title = models.CharField(max_length=100, default='blank')
-    about_text = models.TextField(max_length=500, default='blank')
+    about_title = models.CharField(max_length=100, blank=True)
+    about_text = models.TextField(max_length=500, blank=True)
     about_link = models.CharField(max_length=150)
     about_disc = models.CharField(max_length=100)
     news_id = models.OneToOneField(News, on_delete=models.CASCADE, blank=True)
     projects_id = models.OneToOneField(Project, on_delete=models.CASCADE, blank=True)
-    contact_title = models.CharField(max_length=100, default='blank')
-    contact_link_area = models.CharField(max_length=200, default='blank')
-    contact_button = models.CharField(max_length=50, default='blank')
-    address_title = models.CharField(max_length=50, default='blank')
-    address = models.CharField(max_length=50, default='blank')
-    email_title = models.CharField(max_length=50, default='blank')
+    contact_title = models.CharField(max_length=100, blank=True)
+    contact_link_area = models.CharField(max_length=200, blank=True)
+    contact_button = models.CharField(max_length=50, blank=True)
+    address_title = models.CharField(max_length=50, blank=True)
+    address = models.CharField(max_length=50, blank=True)
+    email_title = models.CharField(max_length=50, blank=True)
     email = models.EmailField(max_length=300, blank=True)
-    phone_title = models.CharField(max_length=50, default='blank')
+    phone_title = models.CharField(max_length=50, blank=True)
     phone = PhoneNumberField(blank=True)
 
     class Meta:
