ea2920cbac5eaafe7383d5dd75d91337e9285407
isnou <nouioua0islem@gmail.com>
2021-12-23 17:17:50 +0100

    dff

--- front/admin.py
+++ front/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import SiteProfile, Project, News# , SiteManager
+from .models import Profile, Project, News# , SiteManager
 
 
-admin.site.register(SiteProfile)
+admin.site.register(Profile)
 admin.site.register(Project)
 admin.site.register(News)
 # admin.site.register(SiteManager)
