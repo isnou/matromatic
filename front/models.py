e269f6c7a82e52cfe67bc2a4f49fddcb83c38c2f
isnou <nouioua0islem@gmail.com>
2021-12-23 16:30:08 +0100

    def

--- front/models.py
+++ front/models.py
@@ -44,7 +44,7 @@ class Profile(models.Model):
     about_text = models.TextField(max_length=500, blank=True)
     about_link = models.CharField(max_length=150)
     about_disc = models.CharField(max_length=100)
-    news_id = models.OneToOneField(News, on_delete=models.CASCADE, blank=True)
+    news_id = models.OneToOneField(News, on_delete=models.CASCADE, default=1)
     projects_id = models.OneToOneField(Project, on_delete=models.CASCADE, blank=True)
     contact_title = models.CharField(max_length=100, blank=True)
     contact_link_area = models.CharField(max_length=200, blank=True)
