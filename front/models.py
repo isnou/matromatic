26e852860f45a0cfe35c2cc5314063b576727edc
isnou <nouioua0islem@gmail.com>
2021-12-23 01:15:21 +0100

    hell

--- front/models.py
+++ front/models.py
@@ -9,13 +9,12 @@ class Profile(models.Model):
     projects = models.CharField(max_length=50)
     news = models.CharField(max_length=50)
     contact = models.CharField(max_length=50)
-    intro_big_text = models.CharField(max_length=200)
-    intro_small_text = models.CharField(max_length=500)
+    intro_big_text = models.TextField(max_length=200, default='blank')
+    intro_small_text = models.TextField(max_length=500, default='blank')
     intro_button = models.CharField(max_length=50)
     about_title = models.CharField(max_length=100, default='blank')
-    about_text = models.CharField(max_length=500)
+    about_text = models.TextField(max_length=500, default='blank')
     about_link = models.CharField(max_length=150)
     about_disc = models.CharField(max_length=100)
 
 class SiteManager(models.Model):
-    profile_id = models.IntegerField()
