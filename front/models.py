896ea4a0a3b4cb46c06bb2672cd66845f083fd10
isnou <nouioua0islem@gmail.com>
2021-12-23 03:17:10 +0100

    rectf

--- front/models.py
+++ front/models.py
@@ -1,29 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 # user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-class Profile(models.Model):
-    profile_name = models.CharField(max_length=50, default='blank')
-    about = models.CharField(max_length=50)
-    projects = models.CharField(max_length=50)
-    news = models.CharField(max_length=50)
-    contact = models.CharField(max_length=50)
-    intro_big_text = models.TextField(max_length=200, default='blank')
-    intro_small_text = models.TextField(max_length=500, default='blank')
-    intro_button = models.CharField(max_length=50)
-    about_title = models.CharField(max_length=100, default='blank')
-    about_text = models.TextField(max_length=500, default='blank')
-    about_link = models.CharField(max_length=150)
-    about_disc = models.CharField(max_length=100)
-    today_news = models.OneToOneField(News, on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name = "profile"
-
-    def __str__(self):
-        return self.profile_name
 
 class Project(models.Model):
     project_name = models.CharField(max_length=50)
@@ -47,5 +26,26 @@ class News(models.Model):
 
     def __str__(self):
         return self.news_name
+
+class Profile(models.Model):
+    profile_name = models.CharField(max_length=50, default='blank')
+    about = models.CharField(max_length=50)
+    projects = models.CharField(max_length=50)
+    news = models.CharField(max_length=50)
+    contact = models.CharField(max_length=50)
+    intro_big_text = models.TextField(max_length=200, default='blank')
+    intro_small_text = models.TextField(max_length=500, default='blank')
+    intro_button = models.CharField(max_length=50)
+    about_title = models.CharField(max_length=100, default='blank')
+    about_text = models.TextField(max_length=500, default='blank')
+    about_link = models.CharField(max_length=150)
+    about_disc = models.CharField(max_length=100)
+    today_news = models.OneToOneField(News, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "profile"
+
+    def __str__(self):
+        return self.profile_name
 
 # class SiteManager(models.Model):
