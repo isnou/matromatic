ae39b6e986651709a2d5684e6981865d7a2d50b6
isnou <nouioua0islem@gmail.com>
2021-12-23 04:07:14 +0100

    set

--- matromatic/urls.py
+++ matromatic/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import include, path
+from matromatic import settings
 
 urlpatterns = [
-    path('', include('front.urls')),
-    path('admin/', admin.site.urls),
-]
+                  path('', include('front.urls')),
+                  path('admin/', admin.site.urls),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
