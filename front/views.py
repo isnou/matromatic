1256b7659d378d5cdca2c66222989626cfa1f62d
isnou <nouioua0islem@gmail.com>
2022-02-01 20:38:24 +0100

    context

--- front/views.py
+++ front/views.py
@@ -10,13 +10,16 @@ def index(request):
         profile = Profile.objects.get(pk=1)
     except Profile.DoesNotExist:
         raise Http404("Profile does not exist")
+    context = {
+        'profile': profile
+    }
 
-    return render(request, 'base.html', {'profile': profile})
+    return render(request, 'base.html',context)
 
 def projects(request):
     try:
-        project = Profile.projects_id.objects.all()
-    except Profile.DoesNotExist:
-        raise Http404("Profile does not exist")
+        project = project.projects_id.objects.all()
+    except Project.DoesNotExist:
+        raise Http404("Project does not exist")
 
     return render(request, 'project.html', {'project': project})
