680e3488bbc0017ab828e3b67313770c2ad11a4e
isnou <nouioua0islem@gmail.com>
2022-05-04 11:42:41 +0100

    new

--- front/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Project, News# , SiteManager
-
-
-admin.site.register(Profile)
-admin.site.register(Project)
-admin.site.register(News)
-# admin.site.register(SiteManager)
