aa8e9157d6ef780b139f295e473cfbe34ff996a4
isnou <nouioua0islem@gmail.com>
2022-05-04 13:00:24 +0100

    git

--- src/matromatic/urls.py
+++ src/matromatic/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import include, path
-from matromatic import settings
+from src.matromatic import settings
 
 urlpatterns = [
                   path('', include('front.urls')),
