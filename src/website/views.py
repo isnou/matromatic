a80c3369e425c5a636e80873260ea96d1445fe0e
isnou <nouioua0islem@gmail.com>
2022-05-04 16:20:34 +0100

    context

--- src/website/views.py
+++ src/website/views.py
@@ -9,36 +9,6 @@ from .models import Project, Service, Client, Team, Skill, Content
 
 def index(request):
 
-    try:
-        content = Content.objects.get(pk=1)
-    except Content.DoesNotExist:
-        raise Http404("Content does not exist")
-
-    try:
-        projects = Project.objects.all()
-    except Project.DoesNotExist:
-        raise Http404("Project does not exist")
-
-    try:
-        services = Service.objects.all()
-    except Service.DoesNotExist:
-        raise Http404("Service does not exist")
-
-    try:
-        clients = Client.objects.all()
-    except Client.DoesNotExist:
-        raise Http404("Client does not exist")
-
-    try:
-        team = Team.objects.all()
-    except Project.DoesNotExist:
-        raise Http404("Team does not exist")
-
-    try:
-        skill = Skill.objects.all()
-    except Skill.DoesNotExist:
-        raise Http404("skill does not exist")
-
     form = ContactUs(request.POST or None)
     if form.is_valid():
         subject = 'Subject:' + form.cleaned_data['subject']
@@ -53,13 +23,4 @@ def index(request):
         except BadHeaderError:
             return HttpResponse('Un en-tête non valide a été détecté.')
 
-    context = {
-        'content': content,
-        'projects': projects,
-        'services': services,
-        'clients': clients,
-        'team': team,
-        'skill': skill
-    }
-
-    return render(request, "base.html", context)
+    return render(request, "base.html")
