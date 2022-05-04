3130b3e6eb6054855495d32303ee14dfd5ace003
isnou <nouioua0islem@gmail.com>
2022-05-04 13:32:31 +0100

    dd

--- matromatic/urls.py
+++ matromatic/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import include, path
-from src.matromatic import settings
+from matromatic import settings
 
 urlpatterns = [
                   path('', include('front.urls')),
