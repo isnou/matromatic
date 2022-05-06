a63627075079070179188b44492964926b12551f
isnou <nouioua0islem@gmail.com>
2022-05-06 04:29:30 +0100

    old sql

--- src/matromatic/settings.py
+++ src/matromatic/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'matromatic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'matromaticdb',
+        'USER':'matro',
+        'PASSWORD':'Pass@&#!6931',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
