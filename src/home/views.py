c0884426fe1cbee21a46b3d603359da6f7a14bac
Zoeza <zahrahabitaa@gmail.com>
2023-11-10 17:53:19 +0100

    Update views.py

--- src/home/views.py
+++ src/home/views.py
@@ -1,4 +1,3 @@
-from django.http import Http404
 from django.shortcuts import render
 from .models import TopPage, Content, Service, OurProcess, Partner, SocialMedia, \
     Footer, Client, Project, Performance
