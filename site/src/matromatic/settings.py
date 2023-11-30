de6a2e59737ede63eaa6bf9f16d0ca6234f52ee9
Zoeza <zahrahabitaa@gmail.com>
2023-11-30 14:21:27 +0100

    Update settings.py

--- site/src/matromatic/settings.py
+++ site/src/matromatic/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'matromatic.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-     'default': {
+    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'matro_db',
         'USER': 'matro_us',
