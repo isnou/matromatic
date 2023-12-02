82cc89be403bb1b949393f0168b85dd6588421d8
Zoeza <zahrahabitaa@gmail.com>
2023-12-02 02:19:56 +0100

    Update admin.py

--- src/manager/admin.py
+++ src/manager/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Home, MainContent, ContactUs, Service, OurProcess, SocialMedia, Client, Project
+from .models import Home, MainContent, Value, OurValues, ContactUs, Service, OurProcess, SocialMedia, Client, Project
 
 # Register your models here.
 admin.site.register(Home)
 admin.site.register(MainContent)
+admin.site.register(Value)
+admin.site.register(OurValues)
 admin.site.register(Service)
 admin.site.register(OurProcess)
 admin.site.register(Project)
