5714b6a4c7f94df10f8ae6bb2bd5b9cbb872cae7
Zoeza <zahrahabitaa@gmail.com>
2024-03-25 03:57:27 +0100

    gg

--- src/matromatic/settings.py
+++ src/matromatic/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-0y(dj*jiwsr9z_$l@g6)gvx%$@e&)&3u+@(yo)h5!ry2$g&6qw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['89.40.4.102']
 
 # Application definition
 
